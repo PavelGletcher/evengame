5c559132a62f0b1cbc3abf869d48d88a844e7bae
Pavel <138882465+PavelGletcher@users.noreply.github.com>
2023-08-21 22:49:30 +0500

    fix click sound

--- scripts/click.js
+++ scripts/click.js
@@ -1,4 +1,4 @@
-const audio = new Audio("/sound/stone.mp3");
+const audio = new Audio("/evengame/sound/stone.mp3");
 const buttons = document.querySelectorAll(".btn");
 const buttonsback = document.querySelectorAll(".btnback");
 
